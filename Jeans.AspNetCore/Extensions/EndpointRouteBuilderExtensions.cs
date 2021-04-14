d50e0a1856044072a249d96879bf16ecb281120d
李建(lijian15) <lijian15@cn.wilmar-intl.com>
2021-04-14 12:19:14 +0800

    gRPC服务批量注入

---
+++ Jeans.AspNetCore/Extensions/EndpointRouteBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Jeans.AspNetCore.Extensions
+{
+    /// <summary>
+    /// Grpc服务批量注入扩展
+    /// </summary>
+    public static class EndpointRouteBuilderExtensions
+    {
+        public static void MapGrpcServices(this IEndpointRouteBuilder builder)
+        {
+            MethodInfo method = typeof(GrpcEndpointRouteBuilderExtensions).GetMethod("MapGrpcService");
+            IEnumerable<TypeInfo> typeInfos = Assembly.GetEntryAssembly()!.DefinedTypes.Where(t => t.IsClass && !t.IsAbstract && t.BaseType != null && t.BaseType.CustomAttributes.Any(a => a.AttributeType == typeof(BindServiceMethodAttribute)));
+            foreach (TypeInfo item in typeInfos)
+            {
+                method.MakeGenericMethod(item.AsType()).Invoke(null, new object[] { builder });
+            }
+        }
+    }
+}
